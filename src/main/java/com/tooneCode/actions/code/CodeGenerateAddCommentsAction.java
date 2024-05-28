af0e208a83d3c2251a13b7aabce3b4eb3658d8f4
lwinhong <80469181@qq.com>
2024-05-28 20:47:36 +0800

    生成注释提示语修改

--- src/main/java/com/tooneCode/actions/code/CodeGenerateAddCommentsAction.java
+++ src/main/java/com/tooneCode/actions/code/CodeGenerateAddCommentsAction.java
@@ -8,6 +8,6 @@ public class CodeGenerateAddCommentsAction extends CodeGenerateBaseAction {
     @Override
     protected void onActionPerformed(@NotNull AnActionEvent e) {
         var selectedText = getEditorSelectedText(e);
-        SendMessageToPage(e, ChatCodeCmd, selectedText, "为以上代码每一行加上注释");
+        SendMessageToPage(e, ChatCodeCmd, selectedText, "为以上每一行代码加上注释");
     }
 }
